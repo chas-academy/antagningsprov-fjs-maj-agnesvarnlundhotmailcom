950fe811539ebd6c0ed6f86969359ab44f790cad
agnes.varnlund@hotmail.com <agnes.varnlund@hotmail.com>
2025-05-30 14:50:13 +0200

    antagningsprov

--- uppg9.js
+++ uppg9.js
@@ -2,13 +2,23 @@
 
 function uppg9 (){
     // skapa en funktion som heter sort och tar in en array av siffror som argument
+function sort(array){
     // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
-    // i if-sattsen skriv ut i konsolen resultatet
-    // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
-
-    // anropa funktionen och skicka med en array som argument
-
-  
+for (let i = 0; i < array.length; i++){
+    // i if-sattsen skriv ut i konsolen resultatet    
+    if (array[i] % 2 === 0) {
+    // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"        
+    console.log(array[i] + " jämt")
+} else {
+    console.log(array[i] + " udda")
 }
+}
+}
+    // anropa funktionen och skicka med en array som argument
+    sort([3,4,7,10,13])
+
+}
+
+uppg9()
 
 module.exports = { uppg9 };
